ae0e5296c04156ba106b82e4cfc5efbe8d891b02
Maxence Raballand <dev@maxencerb.com>
2024-04-29 17:32:45 +0200

    progress

--- test/contracts/ERC20.sol
+++ test/contracts/ERC20.sol
@@ -1,8 +1,20 @@
-// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-contract ERC20 {
-  constructor() {
-    
+import {ERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+
+contract TestERC20 is ERC20 {
+  uint8 internal immutable _decimals;
+
+  constructor(string memory name_, string memory symbol_, uint8 decimals_) ERC20(name_, symbol_) {
+    _decimals = decimals_;
+  }
+
+  function decimals() public view override returns (uint8) {
+    return _decimals;
+  }
+
+  function mint(address account, uint256 amount) public {
+    _mint(account, amount);
   }
 }
