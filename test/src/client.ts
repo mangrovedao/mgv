8e9050aa62ea180e29016876a3f9450da8f1bcc2
Maxence Raballand <dev@maxencerb.com>
2024-04-30 17:23:29 +0200

    updgrade deps

--- test/src/client.ts
+++ test/src/client.ts
@@ -1,13 +1,25 @@
-import { http, createTestClient, publicActions, walletActions } from 'viem'
-import { privateKeyToAccount } from 'viem/accounts'
-import { foundry } from 'viem/chains'
-import { accounts } from './constants.js'
+import { http, createTestClient, publicActions, walletActions } from "viem";
+import { privateKeyToAccount } from "viem/accounts";
+import { foundry } from "viem/chains";
+import { accounts, poolId } from "./constants.js";
 
 export const globalTestClient = createTestClient({
   chain: foundry,
   transport: http(`http://localhost:${process.env.MAIN_PORT || 8546}`),
-  mode: 'anvil',
+  mode: "anvil",
   account: privateKeyToAccount(accounts[0].privateKey),
 })
   .extend(walletActions)
-  .extend(publicActions)
+  .extend(publicActions);
+
+export function getClient() {
+  return createTestClient({
+    chain: foundry,
+    transport: http(
+      `http://localhost:${process.env.PROXY_PORT || 8545}/${poolId}`
+    ),
+    mode: "anvil",
+  })
+    .extend(walletActions)
+    .extend(publicActions);
+}
