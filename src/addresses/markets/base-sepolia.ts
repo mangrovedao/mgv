54a59ad4260a4d2455e04c4a33761d53a3e913ed
Maxence Raballand <dev@maxencerb.com>
2024-06-10 17:58:43 +0200

    feat: Add WBTC and DAI as base tokens for Sepolia market. (#82)

--- src/addresses/markets/base-sepolia.ts
+++ src/addresses/markets/base-sepolia.ts
@@ -1,10 +1,20 @@
 import type { MarketParams } from '../../types/index.js'
-import { baseSepoliaUSDC, baseSepoliaWETH } from '../tokens/base-sepolia.js'
+import {
+  baseSepoliaDAI,
+  baseSepoliaUSDC,
+  baseSepoliaWBTC,
+  baseSepoliaWETH,
+} from '../tokens/base-sepolia.js'
 
 export const baseSepoliaMarkets = [
   {
     base: baseSepoliaWETH,
     quote: baseSepoliaUSDC,
+    tickSpacing: 1n,
+  },
+  {
+    base: baseSepoliaWBTC,
+    quote: baseSepoliaDAI,
     tickSpacing: 1n,
   },
 ] as const satisfies MarketParams[]
