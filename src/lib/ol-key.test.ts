d0db23663f6fc4487837de7c4e245f8bc3797f60
Maxence Raballand <dev@maxencerb.com>
2024-06-26 15:07:37 +0200

    feat: Add classic kandelSeeder to tests.

--- src/lib/ol-key.test.ts
+++ src/lib/ol-key.test.ts
@@ -1,6 +1,6 @@
 import { describe, expect, it } from 'vitest'
 import type { OLKey } from '../types/lib.js'
-import { flip } from './ol-key.js'
+import { flip, hash } from './ol-key.js'
 
 describe('flip', () => {
   it('flips the OLKey', () => {
@@ -15,5 +15,62 @@ describe('flip', () => {
       inbound_tkn: '0x1',
       tickSpacing: 100n,
     })
+  })
+
+  it('hashes the olKey', () => {
+    const markets = [
+      {
+        id: '0x456cb2ca75d08a5c881c4b083a6dd4a469f35d14bd7f2279e8ea8141378ad840',
+        outbound_tkn: '0x999f220296b5843b2909cc5f8b4204aaca5341d8',
+        inbound_tkn: '0x4300000000000000000000000000000000000004',
+        tickSpacing: 1n,
+      },
+      {
+        id: '0x8c421ef7d31aa292802bc4360219b78a45e38a40cc76d04f3f0a9fbc914cbd79',
+        outbound_tkn: '0x4300000000000000000000000000000000000003',
+        inbound_tkn: '0x5d3a1ff2b6bab83b63cd9ad0787074081a52ef34',
+        tickSpacing: 1n,
+      },
+      {
+        id: '0x9478fa0733344acd896e7f5ffa9ee03ccab653020a2898458323ddd6b53593df',
+        outbound_tkn: '0x4300000000000000000000000000000000000003',
+        inbound_tkn: '0x4300000000000000000000000000000000000004',
+        tickSpacing: 1n,
+      },
+      {
+        id: '0x9646f837c173ebace85f4331500010baada3c402b39fe0b42cb6a981ae4c86d0',
+        outbound_tkn: '0x5d3a1ff2b6bab83b63cd9ad0787074081a52ef34',
+        inbound_tkn: '0x4300000000000000000000000000000000000003',
+        tickSpacing: 1n,
+      },
+      {
+        id: '0x970524a7911bd72feac6c2e9065d469de667eeb53c26dd876351b6c9eaea9a30',
+        outbound_tkn: '0x9a50953716ba58e3d6719ea5c437452ac578705f',
+        inbound_tkn: '0x4300000000000000000000000000000000000004',
+        tickSpacing: 1n,
+      },
+      {
+        id: '0xa0d8dad9aa31ad878a583f223811d7a37f06a6f2eefaf72f6b60ef43419f9cdd',
+        outbound_tkn: '0x4300000000000000000000000000000000000004',
+        inbound_tkn: '0x4300000000000000000000000000000000000003',
+        tickSpacing: 1n,
+      },
+      {
+        id: '0xa1dbe0263a07f9407dbe0f2b0ccfae710ce701cca7bfba9fe42227998e859403',
+        outbound_tkn: '0x4300000000000000000000000000000000000004',
+        inbound_tkn: '0x9a50953716ba58e3d6719ea5c437452ac578705f',
+        tickSpacing: 1n,
+      },
+      {
+        id: '0xdff65bb4101d518f43ad16acfea56d37bce9a65b8ecc0de94a3e238f68a3516d',
+        outbound_tkn: '0x4300000000000000000000000000000000000004',
+        inbound_tkn: '0x999f220296b5843b2909cc5f8b4204aaca5341d8',
+        tickSpacing: 1n,
+      },
+    ] as const
+
+    for (const market of markets) {
+      expect(hash(market)).toEqual(market.id)
+    }
   })
 })
--- test/globalSetup.ts
+++ test/globalSetup.ts
@@ -7,6 +7,7 @@ import { globalTestClient } from '~test/src/client.js'
 import { accounts } from './src/constants.js'
 import {
   deployERC20,
+  deployKandelSeeder,
   deployMangroveCore,
   deployMangroveOrder,
   deployMangroveReader,
@@ -18,6 +19,7 @@ import {
 import { kandellibBytecode } from './src/contracts/kandellib.bytecode.js'
 import { getMangroveBytecodes } from './src/contracts/mangrove.js'
 import { smartKandelSeederBytecode } from './src/contracts/smart-kandel-seeder.bytecode.js'
+import { kandelSeederBytecode } from './src/contracts/kandel-seeder.bytecode.js'
 
 export const multicall: Address = '0xcA11bde05977b3631167028862bE2a173976CA11'
 
@@ -79,6 +81,13 @@ export default async function ({ provide }: GlobalSetupContext) {
     smartKandelSeederBytecode,
   )
 
+  const kandelSeeder = await deployKandelSeeder(
+    mangrove,
+    128_000n,
+    kandelLib,
+    kandelSeederBytecode,
+  )
+
   provide('tokens', { WETH, USDC, DAI })
   provide('mangrove', {
     mgv: mangrove,
@@ -89,7 +98,7 @@ export default async function ({ provide }: GlobalSetupContext) {
     multicall,
     tickSpacing: 60n,
   })
-  provide('kandel', { kandelLib, smartKandelSeeder })
+  provide('kandel', { kandelLib, kandelSeeder, smartKandelSeeder })
 
   // open markets
 
@@ -163,6 +172,7 @@ declare module 'vitest' {
     }
     kandel: {
       kandelLib: Address
+      kandelSeeder: Address
       smartKandelSeeder: Address
     }
   }
