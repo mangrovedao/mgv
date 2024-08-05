770e2917a7602b488560913bf90e3a4cfc9845db
Maxence Raballand <dev@maxencerb.com>
2024-08-05 16:48:52 +0200

    fix: token decimals

--- src/addresses/tokens/arbitrum.ts
+++ src/addresses/tokens/arbitrum.ts
@@ -8,6 +8,7 @@ export const arbitrumWETH = buildToken({
 export const arbitrumUSDC = buildToken({
   address: '0xaf88d065e77c8cC2239327C5EDb3A432268e5831',
   symbol: 'USDC',
+  decimals: 6,
   displayDecimals: 2,
   priceDisplayDecimals: 4,
 })
@@ -15,6 +16,7 @@ export const arbitrumUSDC = buildToken({
 export const arbitrumUSDT = buildToken({
   address: '0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9',
   symbol: 'USDT',
+  decimals: 6,
   displayDecimals: 2,
   priceDisplayDecimals: 4,
 })
