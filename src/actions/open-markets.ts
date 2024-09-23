a701c86d67aba6aad4dba404fc088e289bbddf1e
belbazanas <belbazanas@gmail.com>
2024-09-23 16:39:29 +0200

    resolve merge conflict
    Signed-off-by: belbazanas <belbazanas@gmail.com>

--- src/actions/open-markets.ts
+++ src/actions/open-markets.ts
@@ -1,26 +1,26 @@
 // import type { SimulationParams } from '~mgv/types/actions/simulation.js'
 // import type { openMarketsABI } from '../builder/open-markets.js'
-import type { Client } from "viem";
-import { multicall } from "viem/actions";
+import type { Client } from 'viem'
+import { multicall } from 'viem/actions'
 
-import type { OpenMarketsResult } from "~mgv/types/actions/open-markets.js";
-import type { MangroveActionsDefaultParams } from "~mgv/types/index.js";
+import type { OpenMarketsResult } from '~mgv/types/actions/open-markets.js'
+import type { MangroveActionsDefaultParams } from '~mgv/types/index.js'
 import {
   openMarketsParams,
   parseOpenMarketResult,
-} from "../builder/open-markets.js";
-import { getAction } from "../utils/getAction.js";
+} from '../builder/open-markets.js'
+import { getAction } from '../utils/getAction.js'
 
 export async function getOpenMarkets(
   client: Client,
-  actionParams: MangroveActionsDefaultParams
+  actionParams: MangroveActionsDefaultParams,
 ): Promise<OpenMarketsResult> {
-  const { mgvReader } = actionParams;
+  const { mgvReader } = actionParams
 
   const result = await getAction(
     client,
     multicall,
-    "multicall"
+    'multicall',
   )({
     contracts: [
       {
@@ -29,12 +29,12 @@ export async function getOpenMarkets(
       },
     ],
     allowFailure: false,
-  });
+  })
 
   const parsedOpenMarkets = await parseOpenMarketResult({
     client,
     result: result[0],
-  });
+  })
 
-  return parsedOpenMarkets;
+  return parsedOpenMarkets
 }
