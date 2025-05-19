5b3cf70e8541465fa9f11652f991646436b69cc6
Maxence Raballand <dev@maxencerb.com>
2025-05-19 14:51:05 +0200

    fix: wrong sei address (#168)

--- src/addresses/mangrove/sei.ts
+++ src/addresses/mangrove/sei.ts
@@ -4,6 +4,6 @@ export const seiMangrove = {
   mgv: '0xD9834d7caA2ACf81C40e7AAC645Cf9a57cb14bcd',
   mgvOrder: '0x5bA0761FF644560529B0F54362C8f6024175928b',
   mgvReader: '0xfeafb31AC7f09892B50c4d6DA06a1e48D487499E',
-  smartRouter: '0xCC9D10431f168598AE331f149856E5aEF4AC12dd',
+  smartRouter: '0x81A15AD7d900eD57301FFcB3937cDE056a1acE4D',
   routerProxyFactory: '0x91D001E0b1f3E5CD50AA706896Eb22F024516d93',
 } as const satisfies MangroveActionsDefaultParams
